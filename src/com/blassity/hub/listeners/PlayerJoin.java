ce17ff5b7faea1faa215e8b80cf8489258c709d7
RobinMC <robinslot@hotmail.nl>
2016-06-26 21:48:23 +0200

    Teleport player to spawn on join

--- src/com/blassity/hub/listeners/PlayerJoin.java
+++ src/com/blassity/hub/listeners/PlayerJoin.java
@@ -1,6 +1,10 @@
 package com.blassity.hub.listeners;
 
-import static org.bukkit.ChatColor.*;
+import static org.bukkit.ChatColor.GOLD;
+import static org.bukkit.ChatColor.GRAY;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,6 +17,9 @@ public class PlayerJoin implements Listener {
 		Player player = event.getPlayer();
 		String pn = player.getName();
 		event.setJoinMessage(GOLD + ">> " + GRAY + pn + GOLD + " has joined the server");
+		
+		Location spawn = new Location(Bukkit.getWorld("Hub"), 0, 32, 0);
+		player.teleport(spawn);
 	}
 
 }
