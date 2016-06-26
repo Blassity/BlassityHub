2b67a861dac4451dda5581d74842a32842a00b89
RobinMC <robinslot@hotmail.nl>
2016-06-26 21:51:59 +0200

    Added /host

--- src/com/blassity/hub/Main.java
+++ src/com/blassity/hub/Main.java
@@ -5,6 +5,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.blassity.hub.commands.Host;
 import com.blassity.hub.listeners.PlayerJoin;
 
 public class Main extends JavaPlugin {
@@ -12,6 +13,8 @@ public class Main extends JavaPlugin {
 	@Override
 	public void onEnable(){
 		registerListener(new PlayerJoin());
+		
+		getCommand("host").setExecutor(new Host());
 	}
 	
 	@Override
---
+++ src/com/blassity/hub/commands/Host.java
@@ -0,0 +1,16 @@
+package com.blassity.hub.commands;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Host implements CommandExecutor {
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		sender.sendMessage(ChatColor.GOLD + "Blassity" + ChatColor.GRAY + " >> " + ChatColor.YELLOW +  "This server is hosted by RobinMC.");
+		return true;
+	}
+
+}
