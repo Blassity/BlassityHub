75b37a492a943aea8f2651b9ce4de765adb45e58
RobinMC <robinslot@hotmail.nl>
2016-06-26 22:02:52 +0200

    Cancel damage

---
+++ src/com/blassity/hub/listeners/EntityDamage.java
@@ -0,0 +1,19 @@
+package com.blassity.hub.listeners;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+public class EntityDamage implements Listener {
+	
+	@EventHandler
+	public void onDamageTaken(EntityDamageEvent event){
+		Entity entity = event.getEntity();
+		if (entity instanceof Player){
+			event.setCancelled(true);
+		}
+	}
+
+}
