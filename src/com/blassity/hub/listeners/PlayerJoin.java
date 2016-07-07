1ff636d9ce911824d56c6676bcea6eea08164d7e
RobinMC <robinslot@hotmail.nl>
2016-07-07 20:14:39 +0200

    Moved spawn location into seperate class (2)

--- src/com/blassity/hub/listeners/PlayerJoin.java
+++ src/com/blassity/hub/listeners/PlayerJoin.java
@@ -3,12 +3,13 @@ package com.blassity.hub.listeners;
 import static org.bukkit.ChatColor.GOLD;
 import static org.bukkit.ChatColor.GRAY;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+
+import com.blassity.hub.Var;
 
 public class PlayerJoin implements Listener {
 	
@@ -18,7 +19,7 @@ public class PlayerJoin implements Listener {
 		String pn = player.getName();
 		event.setJoinMessage(GOLD + ">> " + GRAY + pn + GOLD + " has joined the server");
 		
-		Location spawn = new Location(Bukkit.getWorld("Hub"), 0, 32, 0);
+		Location spawn = Var.spawnLocation();
 		player.teleport(spawn);
 	}
 
